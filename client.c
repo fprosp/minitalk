f133da59f9981df9347d2d4f42c73c90e6d84f3b
fprosp <francesco.prosperim@hotmail.com>
2023-03-03 18:22:01 +0100

    si comincia a vedere qualcosa

--- client.c
+++ client.c
@@ -12,3 +12,52 @@
 
 #include "minitalk.h"
 
+void	msg_received(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_printf("Messaggio ricevuto\n");
+	return ;
+}
+
+void bit_dispatcher(int pid, char *str)
+{
+    int strlen;
+    int i;
+    int b;
+
+    strlen = ft_strlen(str);
+    i = 0;
+    while (i < strlen)
+    {
+        b = 7;
+		while (b >= 0)
+		{
+			if ((str[i] >> b) & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			b--;
+			usleep(100);
+		}
+		i++;
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    int pid;
+
+    ft_printf("Use correct of program: \
+            ./client [PID] [STRING TO SEND] ");
+    if (argc != 3 || ft_atoi(argv[1]) < 0)
+    {
+        ft_printf("Invalid arguments, please reload");
+        return (0);
+    }
+    pid = ft_atoi(argv[1]);
+    signal(SIGUSR1, msg_received);
+    bit_dispatcher(pid, argv[2]);
+    usleep (300);
+    return (0);
+}
